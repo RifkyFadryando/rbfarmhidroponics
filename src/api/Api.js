176fcf7057db43586e05d10cf660aa3faa0189e0
RifkyFadryando <90003141+RifkyFadryando@users.noreply.github.com>
2021-09-03 11:55:02 +0700

    Update Api.js

--- src/api/Api.js
+++ src/api/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const Api = axios.create({
     //set default endpoint API
-    baseURL: 'http://rbfarm.freecluster.eu/api'
+    baseURL: 'https://rbhidroponics.web.id/api'
 })
   
 export default Api
